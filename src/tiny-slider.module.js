ef7b8144fbe93f45dbc5d21df1a74efe0d1164a5
Tim Brook <tim@area17.com>
2020-04-22 15:58:47 -0500

    DEV-3731 - remove tabindex from carousel nav wrapper and buttons

--- src/tiny-slider.module.js
+++ src/tiny-slider.module.js
@@ -509,7 +509,7 @@ export var tns = function (options) {
     // fixedWidth or autoWidth while viewportMax is not available
     if (autoWidth || (fixedWidth && !viewportMax)) {
       return slideCount - 1;
-      // most cases 
+      // most cases
     } else {
       var str = fixedWidth ? 'fixedWidth' : 'items',
         arr = [];
@@ -1092,7 +1092,7 @@ export var tns = function (options) {
     // == controlsInit ==
     if (hasControls) {
       if (!controlsContainer && (!prevButton || !nextButton)) {
-        outerWrapper.insertAdjacentHTML(getInsertPosition(options.controlsPosition), '<div class="tns-controls" aria-label="Carousel Navigation" tabindex="0"><button type="button" data-controls="prev" tabindex="-1" aria-controls="' + slideId + '">' + controlsText[0] + '</button><button type="button" data-controls="next" tabindex="-1" aria-controls="' + slideId + '">' + controlsText[1] + '</button></div>');
+        outerWrapper.insertAdjacentHTML(getInsertPosition(options.controlsPosition), '<div class="tns-controls" aria-label="Carousel Navigation"><button type="button" data-controls="prev" aria-controls="' + slideId + '">' + controlsText[0] + '</button><button type="button" data-controls="next" aria-controls="' + slideId + '">' + controlsText[1] + '</button></div>');
 
         controlsContainer = outerWrapper.querySelector('.tns-controls');
       }
